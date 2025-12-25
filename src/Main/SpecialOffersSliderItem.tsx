0801540febaf927aa3c933dd250f47c485f890cd
Liliia Galkina <vvlili@yandex.ru>
2025-12-25 16:16:59 +0300

    испр пути к картинкам

--- src/Main/SpecialOffersSliderItem.tsx
+++ src/Main/SpecialOffersSliderItem.tsx
@@ -28,16 +28,20 @@ const SpecialOffersSliderItem: React.FC<SpecialItemProps> = ({
         <h3 className="card-special__title">{title}</h3>
         <div className="card-special__info">
           <div className="card-special__country">
-            <img src="/image/slider/red.svg" alt="красная капля" />
+            <img src="/Trevland/image/slider/red.svg" alt="красная капля" />
             <p className="card-special__country-name">{country}</p>
           </div>
           <div className="card-special__rating">
-            <img src="/image/slider/star.svg" alt="золотая звездочка" />
-					  <p className="card-special__rating-digits">{rating}</p>
+            <img
+              src="/Trevland/image/slider/star.svg"
+              alt="золотая звездочка"
+            />
+            <p className="card-special__rating-digits">{rating}</p>
           </div>
         </div>
-			  <p className="card-special__duration">{duration}</p>
-        <div className="card-special__price">{price}
+        <p className="card-special__duration">{duration}</p>
+        <div className="card-special__price">
+          {price}
           <span>/person</span>
         </div>
       </div>
